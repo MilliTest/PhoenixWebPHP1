554ce8469adb09bedc9d8755fb568fc18e3961f6
christopher_rains <christopher.rains@milliken.com>
2016-03-17 16:30:14 -0400

    -- MODS --

--- PhoenixWeb/Views/Templates/index.razr.php
+++ PhoenixWeb/Views/Templates/index.razr.php
@@ -8,7 +8,18 @@
         <li>
             <div>
                 <img src="/imgs/products/@( $product.thumbnail ).jpg" alt="@( $product.name )" />
+                @if( $product.reviews > 0 )
                 <p></p>
+                @else
+                <p>
+                    <i class="fa fa-star"></i>
+                    <i class="fa fa-star"></i>
+                    <i class="fa fa-star"></i>
+                    <i class="fa fa-star"></i>
+                    <i class="fa fa-star"></i>
+                    <span>(0)</span>
+                </p>
+                @endif
                 <p class="product-name">@( $product.name )</p>
                 <p>@( $product.price )</p>
             </div>
