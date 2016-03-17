79eeea69ac5a49ff52242371e9ebef359274924a
christopher_rains <christopher.rains@milliken.com>
2016-03-17 11:08:33 -0400

    -- MODS --

--- PhoenixWeb/Views/Masters/master.razr.php
+++ PhoenixWeb/Views/Masters/master.razr.php
@@ -3,6 +3,8 @@
 <head>
     <meta charset="utf-8" />
     <title></title>
+    <link type="text/css" rel="stylesheet" href="/css/reset.min.css" />
+    <link type="text/css" rel="stylesheet" href="/css/theme.css" />
     <style>
         #page-header {
             width: 100%;
