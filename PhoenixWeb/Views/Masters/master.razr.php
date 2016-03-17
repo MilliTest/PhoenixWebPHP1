620d993a8ac4c545e7cf7f0e5dbd89c07c454ef2
christopher_rains <christopher.rains@milliken.com>
2016-03-17 11:30:13 -0400

    -- MODS --

--- PhoenixWeb/Views/Masters/master.razr.php
+++ PhoenixWeb/Views/Masters/master.razr.php
@@ -5,25 +5,16 @@
     <title></title>
     <link type="text/css" rel="stylesheet" href="/css/reset.min.css" />
     <link type="text/css" rel="stylesheet" href="/css/theme.css" />
-    <style>
-        #page-header {
-            width: 100%;
-            height: 100px;
-        }
-        #page-nav {
-            width: 100%;
-            height: 40px;
-            background-color: #ccc;
-        }
-    </style>
 </head>
 <body>
     <header id="page-header">
+        <div class="container">
 
+        </div>
     </header>
-    <nav id="page-nav">
+    <div id="jumbotron">
 
-    </nav>
+    </div>
     @block('contentblock')
     <p>Parent Content</p>
     @endblock
