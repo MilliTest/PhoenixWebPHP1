e1e15729a0a1b857832ef1cd4d3fb388862d5b07
christopher_rains <christopher.rains@milliken.com>
2016-03-18 14:24:24 -0400

    -- MODS --

--- js/details.js
+++ js/details.js
@@ -1,8 +1,30 @@
 (function (window, undefined, $) {
 
+    // Attribution: Stackoverflow
+    // http://stackoverflow.com/questions/995183/how-to-allow-only-numeric-0-9-in-html-inputbox-using-jquery
+    $('input[type="text"]').on('keydown', function (evt) {
+        var key = evt.charCode || evt.keyCode || 0;
+        // allow backspace, tab, delete, enter, arrows, numbers and keypad numbers ONLY
+        // home, end, period, and numpad decimal
+        return (
+            key == 8 ||
+            key == 9 ||
+            key == 13 ||
+            key == 46 ||
+            key == 110 ||
+            key == 190 ||
+            (key >= 35 && key <= 40) ||
+            (key >= 48 && key <= 57) ||
+            (key >= 96 && key <= 105));
+    });
+
     $('form').on('submit', function (evt) {
         evt.preventDefault();
         alert("submitted");
+        var obj = {
+            id: parseInt($('input[type="hidden"]').val(), 10),
+            qty: parseInt($('input[type="text"]').val(), 10)
+        };
     });
 
 }(window, undefined, jQuery));
