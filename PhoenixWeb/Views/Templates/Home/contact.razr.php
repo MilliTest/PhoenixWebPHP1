70238fb6f1b18dd4a063b595fde1185ec452403a
christopher_rains <christopher.rains@milliken.com>
2016-03-18 10:59:36 -0400

    -- MODS --

--- PhoenixWeb/Views/Templates/Home/contact.razr.php
+++ PhoenixWeb/Views/Templates/Home/contact.razr.php
@@ -4,10 +4,10 @@
 <section id="terms-of-use">
     <h1>Contact</h1>
     <h2>SUPPORT</h2>
-    <p><a href="mailto: support&#64;peakoutdooradventuregear.com">support&#64;peakoutdooradventuregear.com</a></p>
+    <p>Problems with a product? <a href="mailto: support&#64;fcd-phoenix-web.azurewebsites.com">support&#64;fcd-phoenix-web.azurewebsites.com</a></p>
     <h2>SALES AND DISTRIBUTION</h2>
-    <p><a href="mailto: sales&#64;peakoutdooradventuregear.com">sales&#64;peakoutdooradventuregear.com</a></p>
+    <p>Interested in becoming a business partner? <a href="mailto: sales&#64;fcd-phoenix-web.azurewebsites.com">sales&#64;fcd-phoenix-web.azurewebsites.com</a></p>
     <h2>GENERAL BUSINESS INQUIRIES</h2>
-    <p><a href="mailto: business@peakoutdooradventuregear.com">business&#64;peakoutdooradventuregear.com</a></p>
+    <p>Questions about our operations? <a href="mailto: business@fcd-phoenix-web.azurewebsites.com">business&#64;fcd-phoenix-web.azurewebsites.com</a></p>
 </section>
 @endblock
