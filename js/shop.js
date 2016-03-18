c297f9745b1686b8e227afbb48b7fb0e6e9ec6d7
christopher_rains <christopher.rains@milliken.com>
2016-03-18 13:03:42 -0400

    -- MODS --

--- js/shop.js
+++ js/shop.js
@@ -1,7 +1,7 @@
 (function (window, undefined, $) {
 
     $('#section-filters div').on('change', 'select', function (evt) {
-        var value = $(this).val(),
+        var value = $(this).val().toLowerCase,
             fragment = "";
         if (value === "") {
             window.location = "/shop";
