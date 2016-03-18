0bc4eda8886c85810ccc4ee32b3466923aa20ccf
christopher_rains <christopher.rains@milliken.com>
2016-03-18 10:54:17 -0400

    -- MODS --

--- PhoenixWeb/Views/Templates/Home/contact.razr.php
+++ PhoenixWeb/Views/Templates/Home/contact.razr.php
@@ -4,7 +4,10 @@
 <section id="terms-of-use">
     <h1>Contact</h1>
     <h2>SUPPORT</h2>
+    <p><a href="mailto: support@peakoutdooradventuregear.com">support@peakoutdooradventuregear.com</a></p>
     <h2>SALES AND DISTRIBUTION</h2>
+    <p><a href="mailto: sales@peakoutdooradventuregear.com">sales@peakoutdooradventuregear.com</a></p>
     <h2>GENERAL BUSINESS INQUIRIES</h2>
+    <p><a href="mailto: business@peakoutdooradventuregear.com">business@peakoutdooradventuregear.com</a></p>
 </section>
 @endblock
